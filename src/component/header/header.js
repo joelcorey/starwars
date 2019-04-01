4ed2dacd824fa4da637fc0946ada4a8adfb69dc2
joelcorey <joelcorey@fastmail.com>
2019-03-31 17:28:21 -0700

    add header

---
+++ src/component/header/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        Star Wars Search
+      </header>
+      
+    );
+  }
+}
+
+export default Header;
