aaaba309778c96441ba5444494f3310a09039d3d
joelcorey <joelcorey@fastmail.com>
2019-04-01 13:28:43 -0700

    add generic fetch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import SearchPeople from './component/SearchPeople/SearchPeople'
 import Header from './component/Header/Header';
 
 class App extends Component {
+  state = {
+    people: [],
+    isLoading: false,
+    isError: null,
+  }
+  fetchApi(endpoint, stateKey = 'people', method = 'GET') {
+    fetch(`http https://swapi.co/api/${endpoint}`, {
+      method: method,
+      headers: {
+        'content-type': 'application-json'
+      },
+    })
+    .then(response => response.json())
+    .then(response => this.setState({[stateKey]: response}))
+    .catch(error => this.setState({isError: error}));
+  }
   render() {
     return (
       <>
